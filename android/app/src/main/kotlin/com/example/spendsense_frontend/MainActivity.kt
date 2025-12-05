dd21d64319f20a81fc4b6f1b11060010ae172c02
apekshaaw <apeksha.wagle2017@gmail.com>
2025-12-06 00:31:33 +0545

    feat: add

---
+++ android/app/src/main/kotlin/com/example/spendsense_frontend/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.spendsense_frontend
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'app/app.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: add service locator / Hive init here later if needed
+  runApp(const SpendSenseApp());
+}
